3536a49883150a96f5edb40d146f4bb434f7ad64
WheresAlice <wheresalice@noreply.github.com>
2017-04-23 17:14:56 +0100

    fix typo in rss link

--- reading.rb
+++ reading.rb
@@ -4,7 +4,7 @@ require 'rss'
 
 get '/:user' do
   xml = File.read('posts.rss') if File.exist?('posts.rss')
-  xml ||= HTTParty.get("https://www.reading.am/#{params[:user]}/post.rss").body
+  xml ||= HTTParty.get("https://www.reading.am/#{params[:user]}/posts.rss").body
   feed = RSS::Parser.parse(xml)
   erb :rss, locals: {
       items: feed.items,
